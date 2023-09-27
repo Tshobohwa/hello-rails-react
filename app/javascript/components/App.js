fbf6b0ad3326f4199d63f25615e8279ee8e3f466
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-09-28 00:55:08 +0200

    USe of routes and provider

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import RandomMessage from './RandomMessage';
+import React from "react";
+import RandomMessage from "./RandomMessage";
+import { Route, Routes } from "react-router";
 
-const App = () => <RandomMessage />;
+const App = () => (
+  <Routes>
+    <Route path="/" element={<RandomMessage />} />
+  </Routes>
+);
 
 export default App;
