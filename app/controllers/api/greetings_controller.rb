ab59f5dfb33df593bdd58ea452e76557463bb8ac
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-09-27 17:00:11 +0200

    Create api endpoints

---
+++ app/controllers/api/greetings_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Api
+  class GreetingsController < ApplicationController
+    def random_greeting
+      random_message = Message.order('RANDOM()').first
+      render json: { greeting: random_message.text }
+    end
+  end
+end
