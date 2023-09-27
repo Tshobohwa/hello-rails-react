b7db38befbda597734fad1241a8e0a4cba118fec
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-09-27 17:31:17 +0200

    Create React entry point

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './component/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />,
+);
